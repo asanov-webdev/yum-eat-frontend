5f342d187f3a6c6d25b98ee1206160914c5e3692
Arthur Asanov <asanovarthur1@gmail.com>
2023-02-12 15:41:26 +0300

    Добавил реализацию поиска (без верстки)

--- src/scenes/Menu/Menu.jsx
+++ src/scenes/Menu/Menu.jsx
@@ -1,24 +1,47 @@
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import classNames from 'classnames'
-
 import 'styles/Menu.scss'
+import debounce from 'lodash.debounce'
+
 import pizzaImg from 'styles/img/pizza.png'
 import searchIcon from 'styles/icons/search.png'
 
 import { DishCard } from './DishCard'
 import { mockCategories, mockDishes } from './mock'
 
+const SEARCH_MIN_LENGTH = 2
+const SEARCH_DELAY_IN_MILLISECONDS = 300
+
 export function Menu() {
     const [activeCategories, setActiveCategories] = useState([])
     const [cart, setCart] = useState({})
+    const [isInSearchMode, setIsInSearchMode] = useState(false)
+    const [searchValue, setSearchValue] = useState('')
+    const [dishesBySearchValue, setDishesBySearchValue] = useState([])
 
-    const currentDishes = useMemo(() => {
+    const dishesByCategory = useMemo(() => {
         if (activeCategories.length > 0) {
             return mockDishes.filter(dish => activeCategories.includes(dish.category))
         }
 
         return mockDishes
     }, [activeCategories])
+
+    const handleSearch = debounce(
+        () => {
+            if (searchValue.length > SEARCH_MIN_LENGTH) {
+                setDishesBySearchValue(
+                    mockDishes.filter(dish => dish.title.toLowerCase().includes(searchValue.toLowerCase())),
+                )
+            } else if (!searchValue) {
+                setDishesBySearchValue([])
+            }
+        },
+        SEARCH_DELAY_IN_MILLISECONDS,
+    )
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => { handleSearch() }, [searchValue])
 
     const addDishToCart = (dishId) => {
         const newCart = { ...cart }
@@ -52,11 +75,52 @@ export function Menu() {
         return totalPrice
     }, [cart])
 
+    const handleExitSearch = () => {
+        setIsInSearchMode(false)
+        setSearchValue('')
+        setDishesBySearchValue([])
+    }
+
+    if (isInSearchMode) {
+        return (
+            <div className="menu-wrapper">
+                <button type="button" onClick={handleExitSearch}>Назад</button>
+                <input type="text" placeholder="Поиск" onChange={(e) => { setSearchValue(e.target.value) }} />
+                {dishesBySearchValue.length > 0 ? (
+                    <div className="dishes">
+                        {dishesBySearchValue.map(dish => (
+                            <DishCard
+                                key={dish.id}
+                                id={dish.id}
+                                title={dish.title}
+                                img={pizzaImg}
+                                cart={cart}
+                                onAdd={addDishToCart}
+                                onRemove={removeDishFromCart}
+                            />
+                        ))}
+                    </div>
+                ) : <p>Введите название блюда</p>}
+                {cartTotalPrice > 0 && (
+                    <div className="footer">
+                        <button type="button">
+                            <span>Корзина</span>
+                            <span>{`${cartTotalPrice} руб.`}</span>
+                        </button>
+                    </div>
+                )}
+            </div>
+        )
+    }
+
     return (
         <div className="menu-wrapper">
             <div className="header">
                 <h1>Меню</h1>
-                <img src={searchIcon} alt="search" />
+                <button onClick={() => { setIsInSearchMode(true) }} type="button">
+                    <img src={searchIcon} alt="search" />
+                </button>
+
             </div>
             <div className="categories">
                 {mockCategories.map(cat => (
@@ -76,7 +140,7 @@ export function Menu() {
                 ))}
             </div>
             <div className="dishes">
-                {currentDishes.map(dish => (
+                {dishesByCategory.map(dish => (
                     <DishCard
                         key={dish.id}
                         id={dish.id}
